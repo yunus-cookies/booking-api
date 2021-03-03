afb00fc3e8252f4e5df5fd08b9768508046a8aab
yunus-cookies <yunusokutan@mail.com>
2021-03-03 01:13:02 +0100

    Small fixes and cleanup

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,22 +1,8 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import React from "react";
+import "./custom.css";
 
-import './custom.css'
+const App = () => {
+  return <div>Hello</div>;
+};
 
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
-}
+export default App;
